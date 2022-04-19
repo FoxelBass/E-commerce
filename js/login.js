a361964bd573594ea2bd1ac5c5c6be24bd6ab53d
David Duartez <duartezdavid@gmail.com>
2022-04-19 18:52:13 -0300

    login script update

---
+++ js/login.js
@@ -0,0 +1,9 @@
+function go(){
+
+    if (document.form.password.value=='contraseña' && document.form.login.value=='usuario'){ 
+            document.form.submit(); 
+        } 
+        else{ 
+             alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+        } 
+    } 
